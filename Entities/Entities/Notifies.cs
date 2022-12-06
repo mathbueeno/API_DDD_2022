e8ec9f68ff9d4d237b3693a2c09b1b3595d85500
mathbueeno <mathbueeno@gmail.com>
2022-12-06 10:42:42 -0300

    Implementação da Message

---
+++ Entities/Entities/Notifies.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entities.Entities
+{
+    public class Notifies
+    {
+        public Notifies()
+        {
+            Notifications = new List<Notifies>();
+
+
+        }
+
+        [NotMapped]
+        public string NomePropriedade { get; set; }
+        
+        [NotMapped]
+        public string Mensagem { get; set; }
+
+        [NotMapped]
+        public List<Notifies> Notifications { get; set; }
+
+        public bool ValidarPropriedadeString(string valor, string nomePropriedade)
+        {
+            if(string.IsNullOrWhiteSpace(valor) || string.IsNullOrWhiteSpace(nomePropriedade))
+            {
+                Notifications.Add(new Notifies
+                {
+                    Mensagem = "Campo Obrigatório",
+                    NomePropriedade = nomePropriedade
+                });
+                return false;
+            }
+            
+            return true;
+        }
+
+        public bool ValidarPropriedadeInt(int valor, string nomePropriedade)
+        {
+            if (valor < 1 || string.IsNullOrWhiteSpace(nomePropriedade))
+            {
+                Notifications.Add(new Notifies
+                {
+                    Mensagem = "Campo Obrigatório",
+                    NomePropriedade = nomePropriedade
+                });
+                return false;
+            }
+
+            return true;
+        }
+
+
+
+    }
+}
