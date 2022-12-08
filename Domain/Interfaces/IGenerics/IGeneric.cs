c2cf160997f22202a70195b6c23be2ad891945f4
mathbueeno <mathbueeno@gmail.com>
2022-12-08 10:46:54 -0300

    Implementação do DataContext

---
+++ Domain/Interfaces/IGenerics/IGeneric.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Interfaces.IGenerics
+{
+    public interface IGeneric<T> where T : class
+    {
+        Task Add(T Objeto);
+        Task Update(T Objeto);
+        Task Delete(T Objeto);
+
+        Task<T> GetEntityById(int Id);
+        Task<List<T>> List();
+    }
+}
---
+++ Infrastructure/Configuration/DataContext.cs
@@ -0,0 +1,25 @@
+﻿using Entities.Entities;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrastructure.Configuration
+{
+    public class DataContext : IdentityDbContext<ApplicationUser>
+    {
+        public DataContext(DbContextOptions<DataContext> options)   : base(options) 
+        {
+        }
+
+        public DbSet<Message> Messages { get; set; }
+        public DbSet<ApplicationUser> ApplicationUsers { get; set; }
+
+        public string ObterStringConexao() 
+        {
+        }
+    }
+}
